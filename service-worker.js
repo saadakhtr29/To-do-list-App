ead020fc7d82735dbfacbf58e33dfaa25e374afa
saadakhtr29 <saadakhtr51@gmail.com>
2024-10-18 23:27:40 +0530

    project update

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'todo-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/index.css',
+    '/index.js',
+    '/images/icon.png'
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
